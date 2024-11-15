ca1325b9f9d1cf5067283a6ea005db0d072312a0
pan_da <vitalis1987swot@gmail.com>
2024-11-15 08:12:05 +0500

    print(course_name,number_of_complete_hw,number_of_hours_spent,time_for_one_task)

--- module_1_3.py
+++ module_1_3.py
@@ -1,3 +1,4 @@
+# днамическая типизация
 name = "Виталий"
 print(name,type(name))
 age = 36
@@ -6,3 +7,10 @@ age = 36+1
 print(age)
 is_student = 36==37-1
 print(is_student,type(is_student))
+
+# Переменные
+number_of_complete_hw = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = 1.5/12
+print(course_name,number_of_complete_hw,number_of_hours_spent,time_for_one_task)
