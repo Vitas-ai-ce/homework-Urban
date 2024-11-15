c786cbe0e54f32f07866ad5d3c33253db9045542
Vitas-ai-ce <vitalis1987swot@gmail.com>
2024-11-16 02:36:41 +0500

    module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,27 +1,31 @@
 # днамическая типизация
 name = "Виталий"
-print(name, type(name))
+    print(name, type(name))
 age = 36
-print(age, type(age))
+    print(age, type(age))
 age = 36 + 1
-print(age)
+    print(age)
 is_student = 36 == 37 - 1
-print(is_student, type(is_student))
+    print(is_student, type(is_student))
 
 # Переменные
 number_of_complete_hw = '12'
 number_of_hours_spent = '1.5'
 course_name = 'Python'
 time_for_one_task = 1.5 / 12
-print(time_for_one_task)
+    print(time_for_one_task)
 time_for_one_task = '0.125'
-print('Курс: ' + course_name ,', всего задач: '+ number_of_complete_hw ,', затрачено часов:'+ number_of_hours_spent ,', среднее_время_выполнения: '+time_for_one_task +' часа')
+    print('Курс: ' + course_name ,\
+    ', всего задач: '+ number_of_complete_hw ,\
+    ', затрачено часов:'+ number_of_hours_spent ,\
+    ', среднее_время_выполнения: '+time_for_one_task +' часа')
 
 # Практическое задание по уроку "Строки и индексация строк"
 example = 'Топинамбур'
-print(example, type(example))
-print(example[0])
-print(example[-1])
-print(example[5:])
-print(example[::-1])
-print(example[1:10:2])  # первый и второй индексозначает начало и конец среза, третий индекс означает шаг пропуска элемента строки
+    print(example, type(example))
+    print(example[0])
+    print(example[-1])
+    print(example[5:])
+    print(example[::-1]
+# первый и второй индекс означает начало и конец среза,третий индекс означает шаг пропуска элемента строки
+    print(example[1:10:2]) 
