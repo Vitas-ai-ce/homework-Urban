509d79e4eb830abd24e0800aa05b5625d952067c
Виталий <vitalis1987swot@gmail.com>
2024-11-17 13:54:00 +0500

    "Организация программ и методы строк"

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+# Практическая работа по уроку "Организация программ и методы строк"
+my_sring = input('Введите ваше имя и отчество')
+print(len(my_sring))
+# Методы строк
+print(my_sring.upper())
+print(my_sring.lower())
+print(my_sring.replace(' ', ''))
+print(my_sring[1])
+print(my_sring [-1])
