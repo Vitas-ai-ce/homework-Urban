4c2baf88b42f435b28a5b844c0e44ccb7ed9687f
Виталий <vitalis1987swot@gmail.com>
2024-11-18 18:36:00 +0500

    "Неизменяемые и изменяемые объекты. Кортежи"

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 1, 2, 'hello', True
+print(immutable_var)
+# immutable_var[2] = 'hi'
+# print(immutable_var)
+# Кортеж - это неизменяемый список!
+mutable_list = [3,4,'coin', False]
+mutable_list [2] = 'money'
+mutable_list .remove(False)
+mutable_list .append("Bitcoin")
+mutable_list .extend(['umi', 2])
+print(mutable_list)
