fab85caa09acd92dba16dc889b6558980d99696c
Виталий <vitalis1987swot@gmail.com>
2024-11-25 00:50:51 +0500

    "Условная конструкция. Операторы if, elif, else"

--- module_2_2.py
+++ module_2_2.py
@@ -7,6 +7,6 @@ if first == second == third:
     print(3)
 elif first == second or second == third or third == first:
     print(2)
-elif not first == second == third:
-    sleep(3) # проба задержки по времени
+else:
+    sleep(3)
     print(0)
