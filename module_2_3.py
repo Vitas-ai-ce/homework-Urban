fa4b9cbae2862ebc12e4b920ddb79a831e1177e9
Виталий <vitalis1987swot@gmail.com>
2024-11-25 00:50:16 +0500

    "Стиль кода часть II. Цикл While"

--- module_2_3.py
+++ module_2_3.py
@@ -1,2 +1,11 @@
-
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while my_list != -1:
+    if i <= -1:
+        print(range(int(my_list[i])))
+        continue
+
+    else:
+        break
+
+
