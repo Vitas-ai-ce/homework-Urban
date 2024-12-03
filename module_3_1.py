c624c85c13b2814ee5eaba41e0a55141d2511327
Виталий <vitalis1987swot@gmail.com>
2024-12-03 21:26:43 +0500

    Домашняя работа по уроку "Пространство имён"

--- module_3_1.py
+++ module_3_1.py
@@ -3,28 +3,31 @@
 # Задача "Счётчик вызовов":
 
 calls = 0
-def count_calls ():
+
+
+def count_calls():
     global calls
     calls += 1
+
+
 def string_info(string):
     a = str(string)
     result = (len(a), a.upper(), a.lower())
     count_calls()
     return result
+
+
 def is_contains(string, list_to_search):
     string = str(string).lower()
     list_to_search = list(list_to_search)
     count_calls()
     for i in range(len(list_to_search)):
         if str(list_to_search[i]).lower() == string:
-            result = True
-            break
-        else:
-            result = False
-            continue
 
-    # noinspection PyUnboundLocalVariable
-    return result
+         return True
+
+    return False
+
 
 
 print(string_info('Solovey'))
