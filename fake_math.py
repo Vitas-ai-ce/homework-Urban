d5a8bed8345523a0d51e1107509e5d08392095e2
Виталий <vitalis1987swot@gmail.com>
2024-12-11 21:34:21 +0500

    Домашняя работа по уроку "Модули и пакеты"

---
+++ fake_math.py
@@ -0,0 +1,12 @@
+
+
+def divide(first, second):
+    frst = first
+    sec = second
+    if  sec == 0:
+        return 'Ошибка'
+    else:
+        result_1 = frst / sec
+        return result_1
+
+
---
+++ true_math.py
@@ -0,0 +1,12 @@
+
+from math import inf
+
+def divide(first, second):
+    fst = first
+    snd = second
+    if  snd == 0:
+        return inf
+    else:
+        res_1 = fst / snd
+        return res_1
+
