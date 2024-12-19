f6bf39810c832294648b835ab747ae4652a3bcd0
Виталий <vitalis1987swot@gmail.com>
2024-12-19 21:28:12 +0500

    Практика 1.1

--- module_4/sortfunc.py
+++ module_4/sortfunc.py
@@ -2,26 +2,64 @@
 
 nums = [5, 6, 2, 1, 3, 4]
 
-def bubble_sort(ls):
-    swapped = True
-    while swapped:
-        swapped = False
-        for i in range(len(ls) - 1):
-            if ls[i] > ls[i +1]:
-                ls[i], ls[i +1] = ls[i + 1], ls[i]
-                swapped = True
-
-
-# bubble_sort(nums)
+# def bubble_sort(ls):
+#     swapped = True
+#     while swapped:
+#         swapped = False
+#         for i in range(len(ls) - 1):
+#             if ls[i] > ls[i +1]:
+#                 ls[i], ls[i +1] = ls[i + 1], ls[i]
+#                 swapped = True
+#
+#
+# # bubble_sort(nums)
+# # print(nums)
+#
+# def selection_sort(ls):
+#     for i in range(len(ls)):
+#         lowest = i
+#         for j in range(i + 1, len(ls)):
+#             if ls[j] < ls[lowest]:
+#                 lowest = j
+#         ls[i], ls[lowest] = ls[lowest], ls[i]
+#
+# selection_sort(nums)
 # print(nums)
+def bubble_sort(ls):
+    # чтобы цикл сработал хотя бы один раз, задаем значение переменной True
+    swap = True
+    while swap:
+        swap = False
+        for i in range(len(ls) - 1):
+            if ls[i] > ls[i + 1]:
+                # меняем элементы местами
+                ls[i], ls[i + 1] = ls[i + 1], ls[i]
+                # меняем значение переменной swap для следующего повтора цикла
+                swap = True
+
 
 def selection_sort(ls):
+    # i - количество отсортированных элементов
     for i in range(len(ls)):
-        lowest = i
-        for j in range(i + 1, len(ls)):
-            if ls[j] < ls[lowest]:
-                lowest = j
-        ls[i], ls[lowest] = ls[lowest], ls[i]
+        # изначально считаем минимальным первый элемент
+        low = i
+        # цикл для перебора неотсортированных элементов
+        for j in range(i+1, len(ls)):
+            if ls[j] < ls[low]:
+                low = j
+        # самый минимальный элемент меняем с первым элементом
+        ls[i],  ls[low] = ls[low], ls[i]
 
-selection_sort(nums)
-print(nums)
+
+def insertion_sort(ls):
+    # Начинаем сортировать со второго элемента, т.к. первый элемент считается отсортированным
+    for i in range(1, len(ls)):
+        item = ls[i]
+        # берем элемент для вставки и сохраняем ссылку на индекс предыдущего элемента
+        j = i - 1
+        # отсортированный кусок списка двигаем вперед, если он больше элемента для всавки
+        while j >= 0 and ls[j] > item:
+            ls[j + 1] = ls[j]
+            j -= 1
+        # вставляем элемент
+        ls[j + 1] = item
