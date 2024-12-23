cd0a4fd3b879ecda3f12eddea8658d83854ec057
Виталий <vitalis1987swot@gmail.com>
2024-12-23 22:24:07 +0500

    module_4

--- module_4/sortfunc.py
+++ module_4/sortfunc.py
@@ -63,3 +63,60 @@ def insertion_sort(ls):
             j -= 1
         # вставляем элемент
         ls[j + 1] = item
+
+
+
+
+
+
+
+import names
+from time import time
+
+def decor(func,*args):
+    ls = args[0]
+    print('получил', ls)
+    start = time()
+    func(*args)
+    stop = time()
+    print('отдал', ls)
+    print('время', stop-start)
+
+#0.2
+def bubble_sort(ls, reverse = False):
+    compare = (lambda x,y : x > y) if not reverse else (lambda x,y : x < y)
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(ls)-1):
+            if compare(ls[i], ls[i+1]):
+                ls[i], ls[i + 1] = ls[i+1], ls[i]
+                swapped = True
+
+
+# 0.03
+def selection_sort(ls):
+    for i in range(len(ls)): # i - кол уже проверенных
+        low = i
+        for j in range(i+1,len(ls)):
+            if ls[low] > ls[j]:
+                low = j
+        ls[low],ls[i] = ls[i],ls[low]
+
+# 0.001
+def quick_sort(ls):
+    if len(ls) <= 1:
+        return ls
+    el = ls[len(ls)//2]
+    l = [i for i in ls if i< el]
+    m = [i for i in ls if i == el]
+    r = [i for i in ls if i > el]
+    return quick_sort(l) + m +  quick_sort(r)
+
+
+
+ls = [names.get_first_name() for i in range(1000)]
+#decor(quick_sort, ls)
+print(ls)
+ls = quick_sort(ls)
+print(ls)
