a0e26fb650813874bc5bb24c5e511552e7122265
Виталий <vitalis1987swot@gmail.com>
2025-01-17 20:41:37 +0500

    Домашнее задание по теме "Try и Except"

---
+++ module_8/module_8_1.py
@@ -0,0 +1,13 @@
+# Домашнее задание по теме "Try и Except"
+# Задание "Программистам всё можно":
+
+def add_everything_up(a, b):
+    try:
+        result = round(a + b, 3)
+    except TypeError:
+        result = str(a) + str(b)
+    return result
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
